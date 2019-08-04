199f9b9667cd9ab74482459d2fcd203ffb4db42d
GaoYuan <archergaoy@gmail.com>
2019-08-04 12:30:15 +0900

    test_for_map

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
-a = 3 
+from functools import reduce 
+a = 3
 print(a)
 a = 4 if 0 else "#"
 print(a)
 
 a = 4 if 1 else "#"
+
+li = reduce (lambda x, y, z : x+y+z , [1,2,3,4,5,6])
+print(li)
+
+
+
 print(a)
